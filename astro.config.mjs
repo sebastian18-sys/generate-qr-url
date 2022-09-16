1b061e145d3def1df627656ddb6344caae04170e
sebastian <sebastian99_19@hotmail.com>
2022-09-16 12:10:51 -0500

    change title, add default qr

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from '@astrojs/tailwind';
 import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
   // ...
   integrations: [tailwind()],
